bb3a86aa3c658d927d6a8ced3fd0c962b5d1dd7e
Adam Johnston <adam.johnston3679@gmail.com>
2015-06-17 13:19:59 +0100

    updated on startup db upsert and minor style tweaks

--- server/byron.js
+++ server/byron.js
@@ -8,26 +8,30 @@
           clientSecret = Meteor.settings.clientSecret,
           byronId      = Meteor.settings.byronId;
 
-      HTTP.get('https://api.foursquare.com/v2/venues/search?intent=global&query=Byron&limit=50&categoryId=' + byronId + '&client_id=' + clientId + '&client_secret=' + clientSecret + '&v=20150612', function (err, res) {
+      HTTP.get('https://api.foursquare.com/v2/venues/search?intent=global&query=byron&categoryId=' + byronId + '&client_id=' + clientId + '&client_secret=' + clientSecret + '&v=20150612', function (err, res) {
         if (err)
           throw new Meteor.Error(err.reason);
 
         var byrons = res.data.response.venues.filter(function (venue) {
-              return venue.categories[0].id === byronId;
+              return venue.categories[0].id === byronId && venue.verified && venue.location.country.toLowerCase() === 'united kingdom';
             });
-
-        byrons.forEach(function (byron) {
-          var photo = HTTP.get('https://api.foursquare.com/v2/venues/' + byron.id + '/photos?limit=1&client_id=' + clientId + '&client_secret=' + clientSecret + '&v=20150612');
+ 
+        if (Byrons.find().count() !== byrons.length) {
           
-          Byrons.update({
-            _id: byron.id
-          }, {$set: {
-            address: byron.location.formattedAddress,
-            photo:   photo.data.response.photos.items[0].prefix + '150x150' + photo.data.response.photos.items[0].suffix,
-            votes:   0,
-            ips:     []
+          byrons.forEach(function (byron, i) {
+            var photos = HTTP.get('https://api.foursquare.com/v2/venues/' + byron.id + '/photos?limit=1&client_id=' + clientId + '&client_secret=' + clientSecret + '&v=20150612'),
+                photo  = photos.data.response.photos.items[0];
+
+            Byrons.upsert(byron.id, {$set: {
+              address: byron.location.formattedAddress,
+              photo:   (photo) ? photo.prefix + '150x150' + photo.suffix : null
+            }, $setOnInsert: {
+              votes: 0,
+              ips:   []
+            }});
           });
-        });
+
+        }
       });
 
       Meteor.methods({
@@ -39,10 +43,11 @@
                 return ip === userIp;
               });
 
-          // if (currentIp)
-          //   return;
+          if (currentIp)
+            return;
           
-          Byrons.update(byronId, {$inc: {votes: vote}, $push: {ips: userIp}});
+          Byrons.update(byronId, {$inc: {votes: vote},
+                                  $push: {ips: userIp}});
         }
       });
     });
